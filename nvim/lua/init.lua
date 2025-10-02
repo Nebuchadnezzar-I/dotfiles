45e5d5859edd19f2075fb52299d1772fac568b8b
Michal Ukropec <michalukropec@Michals-MacBook-Pro.local>
2025-10-02 07:17:03 +0200

    Added stuff

---
+++ nvim/lua/init.lua
@@ -0,0 +1,26 @@
+require("config.set")
+require("config.remap")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git", "clone", "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    spec = { { import = "plugins" } },
+    change_detection = { notify = false },
+})
+
+vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+    pattern = "*.conf",
+    callback = function()
+        vim.opt_local.shiftwidth = 4
+        vim.opt_local.softtabstop = 4
+        vim.opt_local.expandtab = true
+    end,
+})
